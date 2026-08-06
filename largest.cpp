7d072ca501b7830e1c80f56ef812cc5ee5e6d85c
pratik gyandev parse <parsepratik889@gmail.com>
2026-08-06 19:58:43 +0530

    Add files via upload

---
+++ largest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    
+    if(a > b)
+        cout << a << " is largest";
+    else if(b > a)
+        cout << b << " is largest";
+    else
+        cout << "Both are equal";
+    
+    return 0;
+}
